15090cf03ec32d60978655cf14704ab982c3b6bd
Djair Maynart <djairmaynart@bol.com.br>
2022-08-11 17:31:02 -0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+import java.lang.Math;
+import java.util.Random;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		String num;
+		
+		System.out.println("Escolha a forma de calcular pi:");
+		System.out.println("1. Método de Monte Carlo");
+		System.out.println("2. Fórmula de Leibniz");
+		
+		do {
+			num = scan.next();
+			
+			if(num.equals("1")) {
+				monteCarlo();
+                scan.close();
+			}else if(num.equals("2")) {
+				System.out.println("2");
+                scan.close();
+			}else {
+				System.out.println("Digite 1 ou 2:");
+			}
+			
+		}while(!num.equals("1") && !num.equals("2"));
+		
+	}
+
+    public static void monteCarlo(){
+
+        Scanner scan = new Scanner(System.in);
+        int num;
+        System.out.println("Digite a quantidade de pontos:");
+
+        while (!scan.hasNextInt()) {
+			scan.next();
+			System.out.println("Digite um valor válido");
+		}
+		num = scan.nextInt();
+        scan.close();
+
+        Random rand = new Random();
+		
+		double p=0;
+		double q=0;
+		double r=0;
+		for(int i=0; i<num; i++) {
+			q=rand.nextDouble();
+			p=rand.nextDouble();
+			if( Math.sqrt(p*p+q*q) <1) {
+				r+=1;
+			}
+		}
+		r=r*4/num;
+		System.out.println("Aproximação de pi: "+r);
+    }
+
+}
