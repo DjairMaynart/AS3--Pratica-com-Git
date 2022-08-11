0c3cc9897ed9f2af20e873b07caae7c2e9b2694a
Djair Maynart <djairmaynart@bol.com.br>
2022-08-11 17:48:19 -0300

    Update main.java

--- main.java
+++ main.java
@@ -27,4 +27,31 @@ public class main {
 		}while(!num.equals("1") && !num.equals("2"));
 		
 	}
+
+    public static void Leibniz(){
+
+        Scanner scan = new Scanner(System.in);
+        int num;
+        System.out.println("Digite a quantidade de termos:");
+
+        while (!scan.hasNextInt()) {
+			scan.next();
+			System.out.println("Digite um valor válido");
+		}
+		num = scan.nextInt();
+        scan.close();
+		
+		double a=1;
+		double r=0;
+		for(int i=0; i<num; i++) {
+			r+= 1/a;
+			a+=2;
+			r-=1/a;
+			a+=2;
+            System.out.println(r);
+		}
+		r*=4;
+		System.out.println("Aproximação de pi: "+r);
+    }
+
 }
